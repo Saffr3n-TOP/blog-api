8e1701319901102f9df712a604a37d379cc79ea4
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-09 22:39:02 +0300

    Create posts controller template

---
+++ controllers/posts.js
@@ -0,0 +1,71 @@
+// @ts-check
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+exports.postsGet = function (req, res, next) {
+  res.status(200).json({
+    data: {
+      status: res.statusCode,
+      message: 'NOT IMPLEMENTED: Posts GET route'
+    }
+  });
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+exports.postsPost = function (req, res, next) {
+  res.status(200).json({
+    data: {
+      status: res.statusCode,
+      message: 'NOT IMPLEMENTED: Posts POST route'
+    }
+  });
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+exports.postGet = function (req, res, next) {
+  res.status(200).json({
+    data: {
+      status: res.statusCode,
+      message: 'NOT IMPLEMENTED: Post GET route'
+    }
+  });
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+exports.postPut = function (req, res, next) {
+  res.status(200).json({
+    data: {
+      status: res.statusCode,
+      message: 'NOT IMPLEMENTED: Post PUT route'
+    }
+  });
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+exports.postDelete = function (req, res, next) {
+  res.status(200).json({
+    data: {
+      status: res.statusCode,
+      message: 'NOT IMPLEMENTED: Post DELETE route'
+    }
+  });
+};
