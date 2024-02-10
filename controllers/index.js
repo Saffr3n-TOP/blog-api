04c42c174d5f9a92f7f818e873fc9cd2d6973dfe
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-10 18:42:33 +0300

    Implement register route

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,8 @@
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const createError = require('http-errors');
+const User = require('../models/user');
+
 /**
  * @param {import("express").Request} req
  * @param {import("express").Response} res
@@ -26,19 +31,75 @@ exports.login = function (req, res, next) {
   });
 };
 
-/**
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- * @param {import("express").NextFunction} next
- */
-exports.register = function (req, res, next) {
-  res.status(200).json({
-    data: {
-      status: res.statusCode,
-      message: 'NOT IMPLEMENTED: Register POST route'
+exports.register = [
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username is required')
+    .bail()
+    .custom(async (username, { req }) => {
+      const user = await User.findOne({
+        username: new RegExp(`^${username}$`, 'i')
+      })
+        .exec()
+        .catch((err) => new Error(err));
+
+      if (user instanceof Error) {
+        req.body = null;
+        throw user;
+      }
+
+      if (user) {
+        throw new Error('Username already exists');
+      }
+    })
+    .bail(),
+  body('password', 'Password is required').trim().notEmpty(),
+
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  async function (req, res, next) {
+    const err = validationResult(req).array()[0];
+
+    if (err) {
+      return next(createError(req.body ? 400 : 500, err.msg));
     }
-  });
-};
+
+    const hash = await bcrypt
+      .hash(req.body.password, 12)
+      .catch((err) => createError(500, err));
+
+    if (hash instanceof Error) {
+      return next(hash);
+    }
+
+    const user = new User({
+      username: req.body.username,
+      hash
+    });
+
+    const savedUser = await user.save().catch((err) => createError(500, err));
+
+    if (savedUser instanceof Error) {
+      return next(savedUser);
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(200).json({
+        data: {
+          message: `User ${user.username} successfully created`
+        }
+      });
+    });
+  }
+];
 
 /**
  * @param {import("express").Request} req
